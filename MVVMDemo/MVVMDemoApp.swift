2f20ce10db3c097f9b04b066bea6d24fbb2c3d73
CharlesDeda <dedaplays@yahoo.com>
2025-02-11 10:44:38 -0500

    Initial Commit

---
+++ MVVMDemo/MVVMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMDemoApp.swift
+//  MVVMDemo
+//
+//  Created by Nick Deda on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
